04a134be502b7fa7e0551ed02c1626e9c9ea7a2d
armvishal <vishal.gill@armentum.co>
2021-12-21 18:53:11 +0530

    initial commit next js

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import {Button} from "reactstrap";
+
+function CustomButton (props: any){
+    return (
+        <Button
+            color="success"
+            onClick={() => {
+                alert('clicked');
+            }}
+            >
+            {props?.name}
+        </Button>
+    )
+}
+export default CustomButton;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import CustomButton from "../../components/button";
+
+export const Login = (login: any) => {
+return (
+    <>
+    This is my login page
+    <CustomButton name={"login"}/>
+    </>
+)
+}
+
+export default Login;
